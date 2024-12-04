2709cbf4593ec400a9dce0d15aa8b9d64869545f
dancantos <dancantos@gmail.com>
2024-12-04 17:43:37 +1100

    benchmark for smarter but slower puzzle 2

--- src/go/day4/benchmark_test.go
+++ src/go/day4/benchmark_test.go
@@ -29,3 +29,30 @@ func BenchmarkCountMas(b *testing.B) {
 	}
 	anchor = result
 }
+
+// goos: darwin
+// goarch: arm64
+// pkg: github.com/dancantos/advent2024/src/go/day4
+// cpu: Apple M1 Pro
+// BenchmarkCountMasSmartButSlow-10    	    6028	    194865 ns/op	       0 B/op	       0 allocs/op
+func BenchmarkCountMasSmartButSlow(b *testing.B) {
+	var result int
+	for i := 0; i < b.N; i++ {
+		result = _countMas(input)
+	}
+	anchor = result
+}
+
+func _countMas(arr [][]rune) int {
+	m := len(arr)
+	n := len(arr[0])
+	count := 0
+	for i := 1; i < m-1; i++ {
+		for j := 1; j < n-1; j++ {
+			if arr[i][j] == 'A' {
+				count += detectMasSmartButSlow(arr, i, j)
+			}
+		}
+	}
+	return count
+}
