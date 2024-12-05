b9abc0d7e6c056315487fc5b4667f68e58c6a593
dancantos <dancantos@gmail.com>
2024-12-05 18:53:53 +1100

    more fair benchmarks

--- src/go/day5/benchmark_test.go
+++ src/go/day5/benchmark_test.go
@@ -21,11 +21,39 @@ func BenchmarkCountGood(b *testing.B) {
 // goarch: arm64
 // pkg: github.com/dancantos/advent2024/src/go/day5
 // cpu: Apple M1 Pro
-// BenchmarkCountBad-10    	    3920	    357964 ns/op	       0 B/op	       0 allocs/op
+// BenchmarkCountBad-10    	    2486	    485612 ns/op	       0 B/op	       0 allocs/op
 func BenchmarkCountBad(b *testing.B) {
 	var result int
+
+	var cp [][]int
 	for i := 0; i < b.N; i++ {
-		result = sum(arrays, badCounter(rules))
+		b.StopTimer()
+		cp = copyInput(arrays)
+		b.StartTimer()
+		result = sum(cp, badCounter(rules))
 	}
 	anchor = result
+}
+
+// goos: darwin
+// goarch: arm64
+// pkg: github.com/dancantos/advent2024/src/go/day5
+// cpu: Apple M1 Pro
+// BenchmarkCountBadNoMutation-10    	    2329	    514100 ns/op	   49250 B/op	    1005 allocs/op
+func BenchmarkCountBadNoMutation(b *testing.B) {
+	var result int
+	for i := 0; i < b.N; i++ {
+		result = sum(arrays, badCounterNoMutation(rules))
+	}
+	anchor = result
+}
+
+func copyInput(arrays [][]int) [][]int {
+	result := make([][]int, len(arrays))
+	for _, arr := range arrays {
+		cp := make([]int, len(arr))
+		copy(cp, arr)
+		result = append(result, cp)
+	}
+	return result
 }
--- src/go/day5/puzzle2.go
+++ src/go/day5/puzzle2.go
@@ -15,3 +15,15 @@ func badCounter(rules map[int][]int) func(arr []int) int {
 		return 0
 	}
 }
+
+// if an array is not sorted, sort it and return the middle element
+func badCounterNoMutation(rules map[int][]int) func(arr []int) int {
+	sortFn := sort(rules)
+	return func(arr []int) int {
+		if !isSorted(arr, rules) {
+			sorted := slices.SortedFunc(slices.Values(arr), sortFn)
+			return sorted[len(sorted)/2]
+		}
+		return 0
+	}
+}
